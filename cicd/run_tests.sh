f4ebf44a9965b1cd902ba2a01691192b5ced2d41
elton <antonold274@gmail.com>
2026-03-16 21:26:11 +0300

    fix CI

--- cicd/run_tests.sh
+++ cicd/run_tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
 set -e
 
 echo "===== TEST STAGE ====="
 
-echo "Test 1: prime number"
+echo "Running test 1: prime number"
+output1=$(printf "7\n" | ./usr/bin/prime-checker)
+echo "$output1"
+echo "$output1" | grep -qi "prime"
 
-echo 7 | ./usr/bin/prime-checker | grep "prime"
-
-echo "Test 2: non-prime number"
-
-echo 8 | ./usr/bin/prime-checker | grep "not prime"
+echo "Running test 2: non-prime number"
+output2=$(printf "8\n" | ./usr/bin/prime-checker)
+echo "$output2"
+echo "$output2" | grep -qi "not prime"
 
 echo "All tests passed"
